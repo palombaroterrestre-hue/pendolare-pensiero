3305317ecbefc486f24ff4c048c501237829161d
palombaroterrestre-hue <palombaro.terrestre@gmail.com>
2026-01-19 11:42:47 +0100

    Create quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,20 @@
+
+fetch('../data/questions.json')
+  .then(response => response.json())
+  .then(data => {
+    const quiz = data.chapter1;
+    const container = document.getElementById('quiz-container');
+    container.innerHTML = `<p>${quiz.question}</p>`;
+    quiz.options.forEach((opt, index) => {
+      const btn = document.createElement('button');
+      btn.textContent = opt;
+      btn.onclick = () => {
+        if(index === quiz.correct){
+          alert('Corretto! Hai sbloccato il tuo NFT.');
+        } else {
+          alert('Riprova!');
+        }
+      };
+      container.appendChild(btn);
+    });
+  });
